c9b972178c170517501d16a57399df4cf9ba809e
turingkomplete <turingkomplete@gmail.com>
2020-06-01 16:26:02 +0430

    Update Memory.java

--- src/simulator/components/Memory.java
+++ src/simulator/components/Memory.java
@@ -1,22 +1,19 @@
 package simulator.components;
 
-import java.util.ArrayList;
-import java.util.Map;
-
 public class Memory extends Component {
     private Boolean[] memory;
 
     private Memory(String label, Wire... inputs) {
         super(label, inputs);
+        memory = new Boolean[65536];
         for (int i = 0; i < 32; ++i) {
             outputs.add(new Wire(false));
-            memory = new Boolean[65536];
         }
     }
 
     public int address() {
         int temp = 0;
-        for (int i = 1; i <= 16; i++) {
+        for (int i = 1; i <= 16; ++i) {
             if(inputs.size() > i) {
                 if(inputs.get(i).getSignal()) {
                     temp += Math.pow(2, 16 - i);
@@ -27,11 +24,15 @@ public class Memory extends Component {
     }
 
     private void memoryWrite() {
-        //
+        for(int i = 17; i <= 48; ++i) {
+            memory[address() + i - 17] = inputs.get(i).getSignal();
+        }
     }
 
     private void memoryRead(){
-        //
+        for (int i = 0; i < 32; ++i) {
+            getOutput(i).setSignal(memory[address() + i]);
+        }
     }
 
     @Override
