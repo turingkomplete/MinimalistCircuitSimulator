b9963afeb54e914547d103c7ee6c0840b2e090ea
turingkomplete <turingkomplete@gmail.com>
2020-06-06 13:06:03 +0430

    Add Xnor

--- src/simulator/Main.java
+++ src/simulator/Main.java
@@ -2,12 +2,16 @@
 
 package simulator;
 
+import simulator.components.gates.And;
 import simulator.control.*;
 
 public class Main {
 
     public static void main(String[] args) {
         //code for circuit
+
+        Debugger debugger = new Debugger(200);
+        debugger.addTrackItem();
 
         Circuit.startAll();
     }
